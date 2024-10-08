4d3cb0902764df2c434b351a7f0a0ab1f40e9aa6
“[firstname <eekubilay@gmail.com>
2024-10-08 18:07:18 +0300

    gerekli düzenlemeler yapılmıştır

---
+++ assets/js/saatlik-ucret-hesaplama.js
@@ -0,0 +1,6 @@
+let saat= prompt('kaç saat çalıştığını giriniz: ');
+
+let ucretSaat= prompt('saatlik çalışma ücretini giriniz: ');
+
+toplamUcret= alert('alacağınız maaş= ' + saat * ucretSaat + 'TL');
+
